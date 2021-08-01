8a7d9c177da3c1a6e201b10cbeb3a1fad0d068dc
Jaehyun Joo <jaehyunjoo@outlook.com>
2021-08-01 00:50:17 -0400

    fixed issue in subsetting

--- R/subset.R
+++ R/subset.R
@@ -6,8 +6,8 @@ setMethod("[", c("poplin", "ANY", "ANY"), function(x, i, j, ..., drop = TRUE) {
     poplinData(x) <- poplinData(x)[ii, , drop = FALSE]
     if (length(reducedDataList(x)) != 0L) {
       message("'poplinReducedData' slot was reset.")
-      poplinReducedData(x) <- new("DFrame", nrows = length(ii))
     }
+    poplinReducedData(x) <- new("DFrame", nrows = ncol(x))
   }
   if (!missing(j)) {
     jj <- .get_subset_index(j, colnames(x))
@@ -17,9 +17,7 @@ setMethod("[", c("poplin", "ANY", "ANY"), function(x, i, j, ..., drop = TRUE) {
     poplinData(x)[["normalizedDataList"]] <- .subset_columns(
       x, jj, get_slot = poplinData, element = "normalizedDataList"
     )
-    if (missing(i)) {
-      poplinReducedData(x) <- poplinReducedData(x)[jj, , drop = FALSE]
-    }
+    poplinReducedData(x) <- poplinReducedData(x)[jj, , drop = FALSE]
   }
   out <- callNextMethod()
   missingCount(out) <- .get_missing_count(assay(out))

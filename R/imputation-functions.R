ff01835ddb6c10d8c482e83c00c34259337bd177
Jaehyun Joo <jaehyunjoo@outlook.com>
2021-09-16 23:54:00 -0400

    Interface for kNN and randomforest imputation

---
+++ R/imputation-functions.R
@@ -0,0 +1,18 @@
+##' @importFrom VIM kNN
+.knn <- function(x, by = c("feature", "sample"), ...) {
+  by <- match.arg(by)
+  if (by == "feature") {
+    out <- kNN(x, ...)[, 1:ncol(x)]
+    ## VIM package internally converts x as data.table, which drops rownames
+    rownames(out) <- rownames(x)
+  } else {
+    out <- t(kNN(t(x), ...))[1:nrow(x), ]
+    colnames(out) <- colnames(x)
+  }
+  out
+}
+
+##' @importFrom missForest missForest
+.randomforest <- function(x, ...) {
+  missForest(x, ...)$ximp
+}

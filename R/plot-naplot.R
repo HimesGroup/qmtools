e6e1c5a6ed99571e8abe9791f4f14b43858ea8eb
Jaehyun Joo <jaehyunjoo@outlook.com>
2021-11-10 17:36:50 -0500

    Updated naplot to use heatmaply:::arrange_plots as hide_colorbar works now.

--- R/plot-naplot.R
+++ R/plot-naplot.R
@@ -8,68 +8,68 @@ poplin_naplot <- function(x, widths = NULL, heights = NULL,
                  showticklabels = showticklabels,
                  row_dend_left = row_dend_left,
                  return_ppxpy = TRUE, plot_method = "ggplot", ...)
-  ## heatmaply:::arrange_plots(plots = p)
+  heatmaply:::arrange_plots(plots = p, hide_colorbar = hide_colorbar)
   ## temporary issue in arrange_plots in hide_colorbar
-  .arrange_plots(plots = p, widths = widths, heights = heights,
-                 hide_colorbar = hide_colorbar, row_dend_left = row_dend_left)
+  ## .arrange_plots(plots = p, widths = widths, heights = heights,
+  ##                hide_colorbar = hide_colorbar, row_dend_left = row_dend_left)
 }
 
-`%||%` <- function(x, y) {
-  if (is.null(x)) y else x
-}
+## `%||%` <- function(x, y) {
+##   if (is.null(x)) y else x
+## }
 
-.arrange_plots <- function(plots, widths = NULL, heights = NULL,
-                          row_dend_left = FALSE, hide_colorbar = FALSE) {
+## .arrange_plots <- function(plots, widths = NULL, heights = NULL,
+##                           row_dend_left = FALSE, hide_colorbar = FALSE) {
 
-  plots <- plots[!sapply(plots, is.null)]
-  if (!row_dend_left) {
-    plots$p <- plots$p + theme(legend.position = "left")
-  }
-  if (hide_colorbar) {
-    plots$p <- plots$p + theme(legend.position = "none")
-  }
-  plots <- lapply(
-    plots,
-    function(x) x + theme(plot.margin = grid::unit(c(0, 0, 0, 0), "npc"))
-  )
+##   plots <- plots[!sapply(plots, is.null)]
+##   if (!row_dend_left) {
+##     plots$p <- plots$p + theme(legend.position = "left")
+##   }
+##   if (hide_colorbar) {
+##     plots$p <- plots$p + theme(legend.position = "none")
+##   }
+##   plots <- lapply(
+##     plots,
+##     function(x) x + theme(plot.margin = grid::unit(c(0, 0, 0, 0), "npc"))
+##   )
 
-  column_list <- list(plots$py, plots$pc, plots$p)
-  ind_null_col <- sapply(column_list, is.null)
+##   column_list <- list(plots$py, plots$pc, plots$p)
+##   ind_null_col <- sapply(column_list, is.null)
 
-  row1_list <- list(
-    plots$py, heatmaply:::ggplot_empty(), heatmaply:::ggplot_empty()
-  )
-  row2_list <- list(
-    plots$pc, heatmaply:::ggplot_empty(), heatmaply:::ggplot_empty()
-  )
-  row3_list <- list(plots$p, plots$pr, plots$px)
+##   row1_list <- list(
+##     plots$py, heatmaply:::ggplot_empty(), heatmaply:::ggplot_empty()
+##   )
+##   row2_list <- list(
+##     plots$pc, heatmaply:::ggplot_empty(), heatmaply:::ggplot_empty()
+##   )
+##   row3_list <- list(plots$p, plots$pr, plots$px)
 
-  if (row_dend_left) {
-    row3_list <- rev(row3_list)
-    row2_list <- rev(row2_list)
-    row1_list <- rev(row1_list)
-  }
-  plotlist <- c(
-    row1_list,
-    row2_list,
-    row3_list
-  )
+##   if (row_dend_left) {
+##     row3_list <- rev(row3_list)
+##     row2_list <- rev(row2_list)
+##     row1_list <- rev(row1_list)
+##   }
+##   plotlist <- c(
+##     row1_list,
+##     row2_list,
+##     row3_list
+##   )
 
-  nrows <- sum(!ind_null_col)
-  ind_remove_col <- rep(ind_null_col, each = length(plotlist) / 3)
+##   nrows <- sum(!ind_null_col)
+##   ind_remove_col <- rep(ind_null_col, each = length(plotlist) / 3)
 
-  ind_null_row <- sapply(row3_list, is.null)
-  ncols <- sum(!ind_null_row)
-  ind_remove_row <- rep(ind_null_row, length.out = length(plotlist))
-  plotlist <- plotlist[!(ind_remove_row | ind_remove_col)]
+##   ind_null_row <- sapply(row3_list, is.null)
+##   ncols <- sum(!ind_null_row)
+##   ind_remove_row <- rep(ind_null_row, length.out = length(plotlist))
+##   plotlist <- plotlist[!(ind_remove_row | ind_remove_col)]
 
-  default_widths <- heatmaply:::default_dims(plots$px, plots$pr)
-  if (row_dend_left) default_widths <- rev(default_widths)
-  egg::ggarrange(
-    plots = plotlist,
-    ncol = ncols,
-    widths = widths %||% default_widths,
-    heights = heights %||% rev(heatmaply:::default_dims(plots$py, plots$pc))
-  )
-}
+##   default_widths <- heatmaply:::default_dims(plots$px, plots$pr)
+##   if (row_dend_left) default_widths <- rev(default_widths)
+##   egg::ggarrange(
+##     plots = plotlist,
+##     ncol = ncols,
+##     widths = widths %||% default_widths,
+##     heights = heights %||% rev(heatmaply:::default_dims(plots$py, plots$pc))
+##   )
+## }
 

7d6607c05c721b6fc2e3a1739b343b89581acd69
Jaehyun Joo <jaehyunjoo@outlook.com>
2021-11-15 22:24:38 -0500

    Documented poplin-class.

--- R/poplin.R
+++ R/poplin.R
@@ -1,8 +1,30 @@
+##' The poplin class
+##'
+##' The poplin class is designed to process LC/MS data. It is an extension of
+##' the standard SummarizedExperiment class, and supports additional containers
+##' for data in process (e.g., normalization, imputation) via
+##' \code{\link{poplinData}} and dimension reduction results (PCA, PLS-DA) via
+##' \code{\link{poplinReduced}}. A poplin object also can be created by
+##' coercing from a \linkS4class{SummarizedExperiment} object.
+##' @param intensity Peak intensity matrix where rows represent features and
+##'   columns represent samples.
+##' @param ... Arguments passed to the
+##'   \code{\link[SummarizedExperiment]{SummarizedExperiment}} constructor.
+##' @return A poplin object
+##' @author Jaehyun Joo
+##' @examples
+##' nsamp <- 20
+##' nfeature <- 1000
+##' m <- matrix(sample(1:100000, size = 200000), nrow = nsamp, ncol = nfeature)
+##' poplin(m)
+##' ## Coercion from an SummarizedExperiment object
+##' as(se, "poplin")
+##' 
 ##' @export
 ##' @import methods
 ##' @importFrom SummarizedExperiment SummarizedExperiment
 poplin <- function(intensity,  ...) {
-  se <- SummarizedExperiment(list(raw = intensity), ...)
+  se <- SummarizedExperiment(assays = SimpleList(raw = intensity), ...)
   if (!is(se, "SummarizedExperiment")) {
     se <- as(se, "SummarizedExperiment")
   }

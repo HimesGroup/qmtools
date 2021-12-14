d021703b6a34e81e4a9e86a656cbc4b232bcfa00
Jaehyun Joo <jaehyunjoo@outlook.com>
2021-12-14 00:26:40 -0500

    added corplot manual.

--- R/plot-corplot.R
+++ R/plot-corplot.R
@@ -1,23 +1,84 @@
+##' Correlation plot
+##'
+##' Visualize correlations between samples or features. All values in a
+##' correlation matrix are visualized by rectangles.
+##'
+##' @param x A matrix or \linkS4class{poplin} object.
+##' @param poplin_in Name of a data matrix to retrieve.
+##' @param unit A character string specifying whether a correlation matrix is
+##'   computed based on samples or features. One of "sample" or "feature".
+##' @param use A method for computing correlations in the presence of missing
+##'   values. Refer to \code{?cor} for details.
+##' @param method A character string specifying which correlation coefficient is
+##'   to be computed. Refer to \code{?cor} for details.
+##' @param widths Relative widths of heatmap and dendrogram.
+##' @param heights Relative heights of heatmap and dendrogram.
+##' @param colors A vector of colors for heatmap.
+##' @param grid_gap Gap between cells.
+##' @param hide_colorbar Logical controlling whether the color bar (legend) is
+##'   hidden.
+##' @param showticklabels A logical vector of length 2 (x-axis, y-axis). If
+##'   \code{FALSE}, the ticks are removed from the side of the plot.
+##' @param row_dend_left Logical controlling whether the row dendrogram is
+##'   placed on the left on the plot.
+##' @param ... Additional arguments passed to [heatmaply::heatmaply].
+##' @return \code{gtable} of aligned plots.
+##' @name poplin_corplot
+NULL
+
+
 ##' @export
 poplin_corplot <- function(x, ...) {
   UseMethod("poplin_corplot")
 }
 
+##' @rdname poplin_corplot
 ##' @export
 ##' @importFrom heatmaply ggheatmap
-poplin_corplot.default <- function(x,
+poplin_corplot.default <- function(x, unit = c("sample", "feature"),
                      use = c("everything", "all.obs", "complete.obs",
                              "na.or.complete", "pairwise.complete.obs"),
                      method = c("pearson", "kendall", "spearman"),
-                     showticklabels = c(TRUE, TRUE), ...) {
+                     widths = NULL, heights = NULL,
+                     colors = viridis::viridis(n = 256, alpha = 1,
+                                               begin = 0, end = 1,
+                                               option = "viridis"),
+                     grid_gap = 0, 
+                     hide_colorbar = FALSE, showticklabels = c(TRUE, TRUE),
+                     row_dend_left = FALSE, ...) {
+  unit <- match.arg(unit)
   use <- match.arg(use)
   method <- match.arg(method)
+  if (unit == "feature") {
+    x <- t(x)
+  }
   m <- cor(x, use = use, method = method)
-  ggheatmap(m, showticklabels = showticklabels, ...)
+  ggheatmap(m, widths = widths, heights = heights,
+            grid_gap = grid_gap, colors = colors,
+            hide_colorbar = hide_colorbar, showticklabels = showticklabels,
+            row_dend_left = row_dend_left, ...)
 }
 
+
+##' @rdname poplin_corplot
 ##' @export
-poplin_corplot.poplin <- function(x, poplin_in , ...) {
+poplin_corplot.poplin <- function(x, poplin_in , unit = c("sample", "feature"), 
+                                  use = c("everything", "all.obs",
+                                          "complete.obs", "na.or.complete",
+                                          "pairwise.complete.obs"),
+                                  method = c("pearson", "kendall", "spearman"),
+                                  widths = NULL, heights = NULL,
+                                  colors = viridis::viridis(n = 256, alpha = 1,
+                                                            begin = 0, end = 1,
+                                                            option = "viridis"),
+                                  grid_gap = 0, 
+                                  hide_colorbar = FALSE,
+                                  showticklabels = c(TRUE, TRUE),
+                                  row_dend_left = FALSE, ...) {
   m <- .verify_and_extract_input(x, poplin_in)
-  poplin_corplot.default(m, ...)
+  poplin_corplot.default(m, use = use, method = method,
+                         widths = widths, heights = heights,
+                         colors = colors, hide_colorbar = hide_colorbar,
+                         showticklabels = showticklabels,
+                         row_dend_left = row_dend_left, ...)
 }
--- R/plot-naplot.R
+++ R/plot-naplot.R
@@ -7,17 +7,17 @@
 ##' @param x A matrix or \linkS4class{poplin} object.
 ##' @param poplin_in Name of a data matrix to retrieve.
 ##' @param widths Relative widths of heatmap and dendrogram.
-##' @param height Relative height of heatmap and dendrogram.
-##' @param grid_gap Gap between cells.
+##' @param heights Relative heights of heatmap and dendrogram.
 ##' @param colors A vector of colors for heatmap.
+##' @param grid_gap Gap between cells.
 ##' @param hide_colorbar Logical controlling whether the color bar (legend) is
 ##'   hidden.
 ##' @param showticklabels A logical vector of length 2 (x-axis, y-axis). If
-##'   \code{FALSE}, ticks are removed from the side of the plot.
+##'   \code{FALSE}, the ticks are removed from the side of the plot.
 ##' @param row_dend_left Logical controlling whether the row dendrogram is
 ##'   placed on the left on the plot.
 ##' @param ... Additional arguments passed to [heatmaply::heatmaply].
-##' @return gtable of aligned plot.
+##' @return \code{gtable} of aligned plots.
 ##' @name poplin_naplot
 NULL
 
@@ -30,10 +30,11 @@ poplin_naplot <- function(x, ...) {
 ##' @export
 ##' @importFrom heatmaply is.na10 heatmaply
 poplin_naplot.default <- function(x, widths = NULL, heights = NULL,
-                          grid_gap = 1, colors = viridis::viridis(2),
-                          hide_colorbar = TRUE, showticklabels = c(TRUE, FALSE),
-                          row_dend_left = FALSE, ...) {
-  p <- heatmaply(is.na10(x), grid_gap = grid_gap, colors = colors,
+                                  colors = viridis::viridis(2), grid_gap = 1, 
+                                  hide_colorbar = TRUE,
+                                  showticklabels = c(TRUE, FALSE),
+                                  row_dend_left = FALSE, ...) {
+  p <- heatmaply(is.na10(x), colors = colors, grid_gap = grid_gap,
                  showticklabels = showticklabels,
                  row_dend_left = row_dend_left,
                  return_ppxpy = TRUE, plot_method = "ggplot", ...)
@@ -48,13 +49,13 @@ poplin_naplot.default <- function(x, widths = NULL, heights = NULL,
 ##' @rdname poplin_naplot
 ##' @export
 poplin_naplot.poplin <- function(x, poplin_in, widths = NULL, heights = NULL,
-                                 grid_gap = 1, color = viridis::viridis(2),
+                                 color = viridis::viridis(2), grid_gap = 1,
                                  hide_colorbar = TRUE,
                                  showticklabels = c(TRUE, FALSE),
                                  row_dend_left = FALSE, ...) {
   m <- .verify_and_extract_input(x, poplin_in)
   poplin_naplot.default(m, widths = widths, heigths = heights,
-                        grid_gap = grid_gap, color = color,
+                        color = color, grid_gap = grid_gap,
                         hide_colorbar = hide_colorbar,
                         showticklabels = showticklabels,
                         row_dend_left = row_dend_left, ...)

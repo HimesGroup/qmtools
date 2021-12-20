81f9496ef1bc2b92bb19d8ebe7d062be47469285
Jaehyun Joo <jaehyunjoo@outlook.com>
2021-12-19 21:04:27 -0500

    Updated ex data to remove unnecessary package dependence.

--- data-raw/faahko_se.R
+++ data-raw/faahko_se.R
@@ -1,10 +1,12 @@
 ## faahKO SummarizedExperiment object
 library(faahKO)
 library(xcms)
+library(poplin)
 library(usethis)
 
 data(faahko3)
 faahko_se <- quantify(faahko3, filled = FALSE)
+metadata(faahko_se) <- list()
 assay(faahko_se, "raw_filled") <- featureValues(faahko3, filled = TRUE)
 faahko_poplin <- as(faahko_se, "poplin")
 use_data(faahko_se, faahko_poplin, overwrite = TRUE, version = 3)
